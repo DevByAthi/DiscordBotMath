a9dcddfb1b568fcc53dbce8b17d3a5fec11119c8
flubblemolubble <66851249+flubblemolubble@users.noreply.github.com>
2020-06-16 22:34:48 -0400

    Fix to factoring
    Turns out I was wayyyyy overthinking this, and all we need to do to find the list of all pairs of factors that multiply to the desired number is this one easy function. It's actually pretty efficient too, I think.

--- chocolate/factoring.py
+++ chocolate/factoring.py
@@ -2,34 +2,16 @@
 Code for finding all pairs of factors for a given number, n. That is to say, all x,y such that n = x*y.
 '''
 
-from functools import reduce
-
-def naivePrimeFactorization(n):
-    factor_list = []
-
-    for i in range(2, n):
-        if i ** 2 >= n:
-            break
-
-        while n % i == 0:
-            n /= i
-            factor_list.append(i)
-
-    if n > 1:
-        factor_list.append(int(n))
-    return factor_list
-
 def factorPairs(n):
-    factor_list = naivePrimeFactorization(n)
+    i = 2
+    original = n
     pair_list = [(1, n)]
-    for i in range(1, len(factor_list)):
-        fac_1 = factor_list[0] if len(factor_list[:i]) == 1 else reduce(lambda x, y: x*y, factor_list[:i])
-        fac_2 = factor_list[-1] if len(factor_list[i:]) == 1 else reduce(lambda x, y: x*y, factor_list[i:])
-        pair_list.append((fac_1, fac_2))
+    while i**2 < original:
+        if n % i == 0:
+            pair_list.append((i, n//i))
+        i += 1
     return pair_list
 
-
+# For testing purposes
 if __name__ == "__main__":
-    factors_n = naivePrimeFactorization(500000)
-    print(factors_n)
-    print(factorPairs(50000))
+    print(factorPairs(50050))
