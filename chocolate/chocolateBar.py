e63018cd98ea880da5d7b42f01a749b81131237c
Athreya Murali <athreya.murali@gmail.com>
2020-06-17 23:34:19 -0400

    Removed debug print statements

--- chocolate/chocolateBar.py
+++ chocolate/chocolateBar.py
@@ -50,14 +50,12 @@ def breakBar(width, height, desired_area, sequence, spaceLeft=1):
     # Find factors of m that fit in given chocolate bar
     factors_list = [(m_1, m_2) for (m_1, m_2) in factoring.factorPairs(m) if
                     (max(m_1, m_2) <= max(w, h)) and (min(m_1, m_2) <= min(w, h))]
-    print(factors_list)  # DEBUG
 
     # DIVIDE-AND-CONQUER occurs here
 
     # No valid factor pairs could be found, so we must divide-and-conquer
     if len(factors_list) == 0:
         breakConstants = approximateOneBreaks(w, h, m)
-        print(breakConstants)
         new_width = breakConstants["height_preserving_break-width"]
         new_height = breakConstants["width_preserving_break-height"]
 
@@ -68,7 +66,6 @@ def breakBar(width, height, desired_area, sequence, spaceLeft=1):
         # Determine number of breaks needed if we first do a height-preserving one-break
         subproblem_height = breakBar(w - new_width, h, m - (new_width * h), sequence, spaceLeft - 1)
         height_preserving_breaks = 1 + subproblem_height
-        print(width_preserving_breaks, height_preserving_breaks)
         return min(width_preserving_breaks, height_preserving_breaks)
 
     # if there exists a pair of factors of m that fit in the chocolate bar
@@ -84,5 +81,4 @@ def breakBar(width, height, desired_area, sequence, spaceLeft=1):
 
 
 if __name__ == "__main__":
-    print("HI")
     print(breakBar(8, 3, 13, [], 2))
