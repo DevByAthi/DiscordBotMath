128b004bebdc6e768b80eba6bbf3da3d5da32bf5
Athreya Murali <athreya.murali@gmail.com>
2020-06-29 20:15:15 -0400

    Format checking added to sorting function

--- schedule/schedule.py
+++ schedule/schedule.py
@@ -3,6 +3,10 @@
 # Precondition 2: For each tuple (s,f) in events, 9 <= s < f <= 17,
 #   where s and f refer to the start and end times of an event in 24-hr time
 def sortByEndTime(events):
+    if not(checkFormat(events)):
+        return
+
+    # TODO: May have to create sorting function from scratch, check with Dr. Braude
     events.sort(key=lambda x: x[1])
 
 # INVARIANT: events_sorted contains the same tuples as events
@@ -20,6 +24,7 @@ def checkFormat(events):
                 for x in events]
     print(validity)
     return all(validity)
+
 
 if __name__ == '__main__':
     events = [(1, 2), (1.0,2),(111, 20,2),9,'1',(1,'2'),(1.0,2.0),(1,0,'2.0')]
