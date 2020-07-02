3233826832501b748052c9b4780ca9512d2566b3
Athreya Murali <athreya.murali@gmail.com>
2020-07-01 21:30:23 -0400

    Added comments to schedule.py

--- schedule/schedule.py
+++ schedule/schedule.py
@@ -3,19 +3,20 @@
 # Precondition 2: For each tuple (s,f) in events, 9 <= s < f <= 17,
 #   where s and f refer to the start and end times of an event in 24-hr time
 def sortByEndTime(events):
-    if not(checkFormat(events)):
+    if not (checkFormat(events)):
         return
 
-    # TODO: May have to create sorting function from scratch, check with Dr. Braude
+    # TODO: Create sorting function from scratch, according to Dr. Braude (1 Jul 2020)
     events.sort(key=lambda x: x[1])
+
 
 # INVARIANT: events_sorted contains the same tuples as events
 
 # POSTCONDITION: events are sorted by their end time
 
-#==============================================================
+# ==============================================================
 
-# Checks that input list is a list of only tuples
+# Checks that input list is a list of only tuples of floats/ints
 
 def checkFormat(events):
     validity = [(isinstance(x, tuple)) and (len(x) == 2)
@@ -25,7 +26,13 @@ def checkFormat(events):
     print(validity)
     return all(validity)
 
+# ==============================================================
+
+
+
 
 if __name__ == '__main__':
-    events = [(1, 2), (1.0,2),(111, 20,2),9,'1',(1,'2'),(1.0,2.0),(1,0,'2.0')]
+    events = [(1, 2), (1.0, 2), (1.0, 2.0), (5,7), (4,6)]
     print(checkFormat(events))
+    sortByEndTime(events)
+    print(events)
