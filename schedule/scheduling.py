d88059f036a19f540a7c8d70c504a861c0e32748
Athreya Murali <athreya.murali@gmail.com>
2020-07-03 17:03:21 -0400

    Added flattening function for sorting-preprocessing and Section class

--- schedule/scheduling.py
+++ schedule/scheduling.py
@@ -1,23 +1,24 @@
-# from schedule import parse
+from collections import deque
+
+from schedule import parse
+
+
+class Section:
+
+    def __init__(self, course_name, times):
+        self.course_name = course_name
+        self.times = times
+
 
 # Intent: Sort a list of events by their end times
 # Precondition 1: events is a non-empty list of 2-element tuples of floats
 # Precondition 2: For each tuple (s,f) in events, 9 <= s < f <= 17,
 #   where s and f refer to the start and end times of an event in 24-hr time
-def sortByEndTime(events):
-    for event in events:
-        # check that event has start and end times within working hours
-        if not(900 <= event[0] <= event[1] <= 1700):
-            return False
-        # check that time format is valid, with minutes within an hour
-        elif event[0] % 100 > 59 or event[1] % 100 > 59:
-            return False
-
-    print(events)
+def sortByEndTime(courses):
+    print(courses)
     # TODO: Create sorting function from scratch, according to Dr. Braude (1 Jul 2020)
-    events.sort(key=lambda x: x[1])
-
-    return True
+    courses.sort(key=lambda x: x[1])
+    print(courses)
 
 
 # INVARIANT: events_sorted contains the same tuples as events
@@ -26,14 +27,16 @@ def sortByEndTime(events):
 
 # ==============================================================
 
+def flattenCourses(course_name, sessions) -> deque:
+    print(course_name)
+    print(sessions)
+    x = deque()
+    return x
+
+
+# ==============================================================
+
 
 if __name__ == '__main__':
-
-    # convertCourseToTime("1 2 3 4 5 8 5 8")
-
-    print("===============")
-
-    events = [(1, 2), (1.0, 2), (1.0, 2.0), (5,7), (4,6)]
-    # sortByEndTime(convertCourseToTime("1 2 3 4 5 8 5 8"))
-    # sortByEndTime(convertCourseToTime("900 930 1045 1100"))
-    print(events)
+    course_str = "Calc_I 900 945 1201 1320  1030 1115 1345 1500"
+    print(flattenCourses(*parse.convertCourseToTime(course_str)))
