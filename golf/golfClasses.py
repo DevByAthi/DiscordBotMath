2c75dd42042d35b37bdbb6a6c74b15021062510c
Athreya Murali <athreya.murali@gmail.com>
2020-07-14 22:47:45 -0400

    Imported parsing into class functions

--- golf/golfClasses.py
+++ golf/golfClasses.py
@@ -1,18 +1,29 @@
+from golf import parseGolf
+
+
 class GolfBall:
-    def __init__(self, position = (0,0), velocity = (0,0)):
+    def __init__(self, position=(0, 0), velocity=(0, 0)):
         self.position = position
         self.velocity = velocity
 
+
 class GolfGraph:
-    def __init__(self, grid, ball):
+    def __init__(self, grid, ball, maxHits=7):
         self.grid = grid
+        self.ball = ball
         self.path = []
         self.numHits = 0
         self.totalEnergySpent = 0
-        self.ball = ball
 
     def findPaths(self):
         pass
 
     def makeMove(self):
         pass
+
+
+if __name__ == "__main__":
+    grid = parseGolf.readFileIntoArray('sampleGrid1.txt')
+    ball = GolfBall()
+    graph = GolfGraph(grid, ball)
+
