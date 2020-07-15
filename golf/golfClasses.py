18a6cffaec6299579871c6a6ffa2e80efe7ab2fb
Athreya Murali <athreya.murali@gmail.com>
2020-07-14 23:02:38 -0400

    Added custom enum for directions, and method of updating position

--- golf/golfClasses.py
+++ golf/golfClasses.py
@@ -1,10 +1,22 @@
+from enum import Enum
 from golf import parseGolf
+
+class Direction(Enum):
+    UP = (-1,0)
+    DOWN = (1,0)
+    LEFT = (0,-1)
+    RIGHT = (0,1)
 
 
 class GolfBall:
-    def __init__(self, position=(0, 0), velocity=(0, 0)):
+    def __init__(self, position=[0, 0], velocity=[0, 0]):
         self.position = position
         self.velocity = velocity
+
+    def __add__(self, other : Direction):
+        new_row = self.position[0] + other.value[0]
+        new_col = self.position[1] + other.value[1]
+        return GolfBall(position=[new_row, new_col])
 
 
 class GolfGraph:
@@ -24,6 +36,9 @@ class GolfGraph:
 
 if __name__ == "__main__":
     grid = parseGolf.readFileIntoArray('sampleGrid1.txt')
-    ball = GolfBall()
+    ball = GolfBall(position=[5,10])
     graph = GolfGraph(grid, ball)
+
+    newBall = ball + Direction.LEFT
+    print(newBall.position)
 
