12f36c8e33107f35f498871de7477fff392d3a7b
Athreya Murali <athreya.murali@gmail.com>
2020-08-01 21:51:21 -0400

    Added new file for MST algorithm with pseudocode

---
+++ factory/reachAllCustomers.py
@@ -0,0 +1,37 @@
+from factory.graphClasses import *
+from heapq import *
+
+def reachAllCustomers(a_graph, a_potential_factory):
+
+    # Verify a_potential_factory is in fact, a potential factory
+    # Check type
+
+    # Pre-processing to find number of customers in entire town
+    # Note, not all customers can necessarily be reached from the potential factory
+
+    # Create a temporary dictionary visited_nodes for the vertex names that have been added to the tree thus far
+
+    # Create a temporary set tree_edges for the edges used in minimum spanning tree
+
+    # Create copy of a_graph's edge set for purposes of finding MST as a min heap
+
+    # Loop: terminate when all edges in a_graph have been encountered
+
+        # Pop edge with the lowest travel cost from the min heap
+
+        # If this current edge connects to a factory
+        # or potential factory that is not a_potential_factory, skip
+
+        # If this current edge already connects two vertices in the visited_nodes dictionary, skip
+
+        # Add names of vertices connected by current edge to visited_nodes
+
+        # Add edge to tree_edges
+
+    # Return Graph object representing the minimum spanning subtree of a_graph
+    # Create Graph object using visited_nodes and tree_edges
+
+    # This graph connects ONLY a_potential_factory to the customers via mail stations
+    # Note that no other factories or potential factories are connected
+
+    pass
